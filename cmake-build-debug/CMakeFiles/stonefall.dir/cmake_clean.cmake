b2b4405a5aa9e6514e255de0f13a137830b524a2
David Oyeka <david.oyeka@apexcapitalcorp.com>
2018-06-11 15:20:40 -0500

    basic setup :

---
+++ cmake-build-debug/CMakeFiles/stonefall.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/stonefall.dir/main.cpp.o"
+  "stonefall.pdb"
+  "stonefall"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/stonefall.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/stonefall.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for stonefall.
+# This may be replaced when dependencies are built.
